e9808e352451e47d810367166245e355e8de185c
Joanne Chen <joanne_r_chen@yahoo.com>
2021-02-01 12:00:43 +0800

    simple class of CRUD created with 9 passing tests

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+
+class MessageApp {
+    constructor() {
+        this.messages = [];
+    }
+    
+    newId(array){
+        if (array.length > 0) {
+            return array[array.length-1].id + 1;
+        } else {
+            return 1
+        }
+      }
+
+    post(message) {
+        let item = {
+            id: this.newId(this.messages),  
+            content: message,
+            date: new Date()
+        }
+
+        this.messages.push(item);
+        return this.messages;
+    }
+
+    get(id) {
+        return this.messages.filter(message => message.id === id)[0];
+    }
+
+    update(id, update) {
+        let index = this.messages.findIndex(message => message.id === id )
+        this.messages[index].content = update
+    }
+
+    delete(id) {
+        this.messages = this.messages.filter(message => message.id != id)
+        return this.messages
+        
+    }
+
+
+}
+
+export default MessageApp;
